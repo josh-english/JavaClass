41dc08cf98ee4b4d1f3d05e4148054c87083ba3d
josh-english <joshua.c.english@gmail.com>
2019-05-20 12:29:59 -0700

    scanner introduction

--- ReadingUserInput/src/com/joshenglish/Main.java
+++ ReadingUserInput/src/com/joshenglish/Main.java
@@ -6,5 +6,26 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your birth year: ");
+        boolean hasNextInt = scanner.hasNextInt();
+        if(hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            // Deal with reading enter as separate line issue
+            scanner.nextLine();
+
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+
+            int age = 2019 - yearOfBirth;
+            if (age  >= 0 && age <= 120) {
+                System.out.println("Hello " + name);
+                System.out.println("You are " + age + " years old!");
+            } else {
+                System.out.println("Invalid Year of Birth");
+            }
+        } else {
+            System.out.println("Entered non-numeric value for birth year.");
+        }
+        scanner.close();
     }
 }
---
+++ ReadingUserInputChallenge/src/com/joshenglish/Main.java
@@ -0,0 +1,30 @@
+package com.joshenglish;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printSumFromUserInput();
+    }
+
+    private static void printSumFromUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        int validEntries = 0;
+        int sum = 0;
+        while (validEntries < 10) {
+            System.out.println("Enter number #" + (validEntries + 1) + ":");
+            if (scanner.hasNextInt()) {
+                int num = scanner.nextInt();
+                scanner.nextLine();
+                sum += num;
+                validEntries++;
+            }
+            else {
+                scanner.nextLine();
+            }
+        }
+        System.out.println("The sum of your number is: " + sum);
+        scanner.close();
+    }
+}
