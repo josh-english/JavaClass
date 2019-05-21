9b57e17df4c511bbf3242bff78883817ce8f1264
josh-english <joshua.c.english@gmail.com>
2019-05-21 11:42:14 -0700

    oop 1 intro

---
+++ Classes/src/com/joshenglish/Car.java
@@ -0,0 +1,23 @@
+package com.joshenglish;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        if (validModel.equals("carrera") || validModel.equals("commodore")) {
+            this.model = validModel;
+        } else {
+            this.model = "unknown";
+        }
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+}
