1e7f00d1ec2ac01573161017695a7fd7957a9073
josh-english <joshua.c.english@gmail.com>
2019-05-22 10:48:51 -0700

    VipCustomer class and testing in main

--- Constructors/src/com/joshenglish/Main.java
+++ Constructors/src/com/joshenglish/Main.java
@@ -19,5 +19,24 @@ public class Main {
 	    account.makeDeposit(150.65);
 	    account.makeWithdrawal(260);
 	    account.makeWithdrawal(100);
-    }
+
+	    System.out.println();
+	    VipCustomer josh = new VipCustomer("Josh English", 2000.00, "test email");
+	    System.out.println("VipCustomer josh's details:");
+	    System.out.println(josh.getName());
+	    System.out.println(josh.getCreditLimit());
+	    System.out.println(josh.getEmailAddress());
+
+	    VipCustomer john = new VipCustomer("John Tester", 1000.00);
+		System.out.println("VipCustomer john's details:");
+		System.out.println(john.getName());
+		System.out.println(john.getCreditLimit());
+		System.out.println(john.getEmailAddress());
+
+		VipCustomer defaulty = new VipCustomer();
+		System.out.println("VipCustomer defaulty's details:");
+		System.out.println(defaulty.getName());
+		System.out.println(defaulty.getCreditLimit());
+		System.out.println(defaulty.getEmailAddress());
+	}
 }
