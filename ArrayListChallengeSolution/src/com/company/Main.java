c1beee752b5e25397b6be8628d9f27bfe358cd95
josh-english <josh@jlap.localdomain>
2019-06-10 11:07:54 -0700

    adding bugfixes for array list challenge solution

--- ArrayListChallengeSolution/src/com/company/Main.java
+++ ArrayListChallengeSolution/src/com/company/Main.java
@@ -38,6 +38,8 @@ public class Main {
                 case 6:
                     printActions();
                     break;
+                default:
+                    break;
             }
         }
     }
